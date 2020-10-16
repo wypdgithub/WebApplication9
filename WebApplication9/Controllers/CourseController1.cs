b4cfb236a1e5f91fa724ad8122f44ec3e70323f1
xuezhang11 <2105937858@qq.com>
2020-10-16 10:46:39 +0800

    士大夫十分

---
+++ WebApplication9/Controllers/CourseController1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class CourseController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
